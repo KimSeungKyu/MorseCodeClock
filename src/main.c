9d00feff3e4a0d8ada8b3067fe299ecd03afaa5a
Kim Seung Kyu <kskxyzho@gmail.com>
2014-03-03 03:57:24 -0800

    version 0.2
    Shake AXIS_Z to morse clock.

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 Window *my_window;
 TextLayer *Time_layer, *Title_layer;
 
-static const uint32_t const morse_time[24][20] = {
+static const uint32_t morse_time[24][20] = {
     { 300, 100, 300, 100, 300, 100, 300, 100, 300, 300 },  //0
     { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300 },  //1
     { 100, 100, 100, 100, 300, 100, 300, 100, 300, 300 },  //2
@@ -29,99 +29,82 @@ static const uint32_t const morse_time[24][20] = {
     { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300 ,100, 100, 100, 100, 300, 100, 300, 100, 300, 300 },  //22
     { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300 ,100, 100, 100, 100, 100, 100, 300, 100, 300, 300 },  //23
 };
-
-/*
-from : http://forums.getpebble.com/discussion/8368
-*/
-static int myAtol(const char *s) {
-    const char *p = s, *q;
-    long n = 0;
-    int sign = 1, k = 1;
-  
-    if (p != NULL) {
-        if (*p != '\0') {
-            if ((*p == '+') || (*p == '-')) {
-                if (*p++ == '-') sign = -1;
-            }
-            for (q = p; (*p != '\0'); p++);
-            for (--p; p >= q; --p, k *= 10) n += (*p - '0') * k;
-        }
-    }
-    return n * sign;
-}
-
-static void handle_minute_tick(struct tm* tick_time, TimeUnits units_changed){
-  
-   static char time_text[] = "00:00";
-
-  strftime(time_text, sizeof(time_text), "%T", tick_time);
-  text_layer_set_text(Time_layer, time_text);
-  
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "minute .... %s", time_text);
-}
-
-// Viberate per hour
-static void handle_hour_tick(struct tm* tick_time, TimeUnits units_changed){
-  
-  static char time_text[] = "00:00";
-  strftime(time_text, sizeof(time_text), "%T", tick_time);
-  text_layer_set_text(Time_layer, time_text);
-  
-  static char morse_number[] = "00"; 
-  strftime(morse_number, sizeof(morse_number), "%T", tick_time);
-  
-  int h = myAtol(morse_number);
+// viberation
+void vibeMorseCode(struct tm* time){
+  int h = time->tm_hour;
   VibePattern pat = {
     .durations = morse_time[h], 
     .num_segments = ARRAY_LENGTH(morse_time[h])
   };
   vibes_enqueue_custom_pattern(pat);
-  
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "hour .... %d", h);
 }
-
 static void handle_tick(struct tm* tick_time, TimeUnits units_changed){  
   if(units_changed & HOUR_UNIT){
-    handle_hour_tick(tick_time, HOUR_UNIT);
+    static char time_text[] = "00:00";
+    strftime(time_text, strlen(time_text)+1, "%T", tick_time);
+    text_layer_set_text(Time_layer, time_text);
+    vibeMorseCode(tick_time);
+    
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "hour .... %d", tick_time->tm_hour);
   }else if(units_changed & MINUTE_UNIT){
-    handle_minute_tick(tick_time, MINUTE_UNIT);
+    static char time_text[] = "00:00";
+
+    strftime(time_text, strlen(time_text)+1, "%T", tick_time);
+    text_layer_set_text(Time_layer, time_text);
+    
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "minute .... %s", time_text);
   }
 }
 
-void handle_init(void) {
-	  my_window = window_create();
-    
-	  Time_layer = text_layer_create(GRect(0, 168-40, 144, 40));
-    text_layer_set_font(Time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-    text_layer_set_text_color(Time_layer, GColorWhite);
-    text_layer_set_background_color(Time_layer, GColorBlack);
-    text_layer_set_text_alignment(Time_layer, GTextAlignmentCenter);
-    
-    time_t now = time(NULL);
-    struct tm *current_time = localtime(&now);
-    handle_tick(current_time, MINUTE_UNIT);
-    tick_timer_service_subscribe(HOUR_UNIT|MINUTE_UNIT, &handle_tick);
-    
-    layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(Time_layer));
+// Current time from tap or shake
+static void handle_tap(AccelAxisType axis, int32_t direction){
+  if(axis != ACCEL_AXIS_Z) return;
   
-    Title_layer = text_layer_create(GRect(0, 0, 144, 35));
-    text_layer_set_font(Title_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-    text_layer_set_text_color(Title_layer, GColorWhite);
-    text_layer_set_background_color(Title_layer, GColorBlack);
-    text_layer_set_text_alignment(Title_layer, GTextAlignmentCenter);
-    text_layer_set_text(Title_layer, "Morse Code Clock");
-    layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(Title_layer));
-    
-    window_stack_push(my_window, true);
+  time_t now = time(NULL);
+  struct tm *current_time = localtime(&now);
+  vibeMorseCode(current_time);
+  
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "now time .... %d:%d", current_time->tm_hour, current_time->tm_min);
+}
+
+void handle_init(void) {
+  my_window = window_create();
+  
+  Time_layer = text_layer_create(GRect(0, 168-40, 144, 40));
+  text_layer_set_font(Time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_text_color(Time_layer, GColorWhite);
+  text_layer_set_background_color(Time_layer, GColorBlack);
+  text_layer_set_text_alignment(Time_layer, GTextAlignmentCenter);
+  
+  time_t now = time(NULL);
+  struct tm *current_time = localtime(&now);
+  handle_tick(current_time, MINUTE_UNIT);
+  tick_timer_service_subscribe(HOUR_UNIT|MINUTE_UNIT, &handle_tick);
+  
+  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(Time_layer));
+  
+  Title_layer = text_layer_create(GRect(0, 0, 144, 35));
+  text_layer_set_font(Title_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_color(Title_layer, GColorWhite);
+  text_layer_set_background_color(Title_layer, GColorBlack);
+  text_layer_set_text_alignment(Title_layer, GTextAlignmentCenter);
+  text_layer_set_text(Title_layer, "Morse Code Clock");
+  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(Title_layer));
+  
+  accel_tap_service_subscribe(&handle_tap);
+  
+  window_stack_push(my_window, true);
 }
 
 void handle_deinit(void) {
-	  text_layer_destroy(Time_layer);
-	  window_destroy(my_window);
+  tick_timer_service_unsubscribe();
+  accel_tap_service_unsubscribe();
+  text_layer_destroy(Time_layer);
+  window_destroy(my_window);
 }
 
 int main(void) {
-	  handle_init();
-	  app_event_loop();
-	  handle_deinit();
+  handle_init();
+  app_event_loop();
+  handle_deinit();
 }
