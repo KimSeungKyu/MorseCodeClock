98cdde92e0b865154511de02a64d142392628313
Kim Seung Kyu <kskxyzho@gmail.com>
2014-03-04 05:01:06 -0800

    version 1.0
    분까지 모스부호로 표현
    조합형 모스부호

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include <pebble.h>
 
 Window *my_window;
-TextLayer *Time_layer, *Title_layer;
+TextLayer *Time_layer, *Morse_layer1, *Morse_layer2, *Morse_layer3, *Morse_layer4;
 
-static const uint32_t morse_time[24][20] = {
+static const uint32_t morse_time[10][10] = {
     { 300, 100, 300, 100, 300, 100, 300, 100, 300, 300 },  //0
     { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300 },  //1
     { 100, 100, 100, 100, 300, 100, 300, 100, 300, 300 },  //2
@@ -14,45 +14,63 @@ static const uint32_t morse_time[24][20] = {
     { 300, 100, 300, 100, 100, 100, 100, 100, 100, 300 },  //7
     { 300, 100, 300, 100, 300, 100, 100, 100, 100, 300 },  //8
     { 300, 100, 300, 100, 300, 100, 300, 100, 100, 300 },  //9
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300, 300, 100, 300, 100, 300, 100, 300, 100, 300, 300 },  //10
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300, 100, 100, 300, 100, 300, 100, 300, 100, 300, 300 },  //11
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300, 100, 100, 100, 100, 300, 100, 300, 100, 300, 300 },  //12
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300, 100, 100, 100, 100, 100, 100, 300, 100, 300, 300 },  //13
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300, 100, 100, 100, 100, 100, 100, 100, 100, 300, 300 },  //14
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300, 100, 100, 100, 100, 100, 100, 100, 100, 100, 300 },  //15
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300, 300, 100, 100, 100, 100, 100, 100, 100, 100, 300 },  //16
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300, 300, 100, 300, 100, 100, 100, 100, 100, 100, 300 },  //17
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300, 300, 100, 300, 100, 300, 100, 100, 100, 100, 300 },  //18
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300, 300, 100, 300, 100, 300, 100, 300, 100, 100, 300 },  //19
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300 ,300, 100, 300, 100, 300, 100, 300, 100, 300, 300 },  //20
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300 ,100, 100, 300, 100, 300, 100, 300, 100, 300, 300 },  //21
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300 ,100, 100, 100, 100, 300, 100, 300, 100, 300, 300 },  //22
-    { 100, 100, 300, 100, 300, 100, 300, 100, 300, 300 ,100, 100, 100, 100, 100, 100, 300, 100, 300, 300 },  //23
 };
+static const char *morse_code[10] = {
+  "- - - - -",  //0
+  "· - - - -",  //1
+  "· · - - -",  //2
+  "· · · - -",  //3
+  "· · · · -",  //4
+  "· · · · ·",  //5
+  "- · · · ·",  //6
+  "- - · · ·",  //7
+  "- - - · ·",  //8
+  "- - - - ·",  //9
+};
+
 // viberation
-void vibeMorseCode(struct tm* time){
-  int h = time->tm_hour;
+static void vibeMorseCode(struct tm* time){
+  short hour = time->tm_hour;
+  short min = time->tm_min;
+  
+  uint32_t now_time[40] = {};
+  short t = 0;
+  for(short i = 0; i < 40; i++){
+    if(i < 10){
+      now_time[i] = morse_time[hour/10][t++];
+    }else if(i < 20){
+      now_time[i] = morse_time[hour%10][t++];
+    }else if(i < 30){
+      now_time[i] = morse_time[min/10][t++];
+    }else{
+      now_time[i] = morse_time[min%10][t++];
+    }
+    if(t==10)t=0;
+  }
+  
   VibePattern pat = {
-    .durations = morse_time[h], 
-    .num_segments = ARRAY_LENGTH(morse_time[h])
+    .durations = now_time, 
+    .num_segments = ARRAY_LENGTH(now_time)
   };
   vibes_enqueue_custom_pattern(pat);
 }
+static void UpdateTime(struct tm* tick_time){
+  static char time_text[] = "000 00 000 00:00";
+  strftime(time_text, strlen(time_text)+1, "%b %d %a %T", tick_time);
+  text_layer_set_text(Time_layer, time_text);
+  
+  text_layer_set_text(Morse_layer1, morse_code[tick_time->tm_hour / 10]);
+  text_layer_set_text(Morse_layer2, morse_code[tick_time->tm_hour % 10]);
+  text_layer_set_text(Morse_layer3, morse_code[tick_time->tm_min / 10]);
+  text_layer_set_text(Morse_layer4, morse_code[tick_time->tm_min % 10]);
+}
 static void handle_tick(struct tm* tick_time, TimeUnits units_changed){  
-  if(units_changed & HOUR_UNIT){
-    static char time_text[] = "00:00";
-    strftime(time_text, strlen(time_text)+1, "%T", tick_time);
-    text_layer_set_text(Time_layer, time_text);
-    vibeMorseCode(tick_time);
-    
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "hour .... %d", tick_time->tm_hour);
-  }else if(units_changed & MINUTE_UNIT){
-    static char time_text[] = "00:00";
+  
+  UpdateTime(tick_time);
 
-    strftime(time_text, strlen(time_text)+1, "%T", tick_time);
-    text_layer_set_text(Time_layer, time_text);
-    
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "minute .... %s", time_text);
+  if(tick_time->tm_min == 0){
+    vibeMorseCode(tick_time);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "MCC: O'clock!");
   }
 }
 
@@ -64,41 +82,56 @@ static void handle_tap(AccelAxisType axis, int32_t direction){
   struct tm *current_time = localtime(&now);
   vibeMorseCode(current_time);
   
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "now time .... %d:%d", current_time->tm_hour, current_time->tm_min);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "MCC: Shake!");
 }
 
 void handle_init(void) {
   my_window = window_create();
+
+  Morse_layer1 = text_layer_create(GRect(0, 0, 144, 140));
+  text_layer_set_font(Morse_layer1, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(Morse_layer1, GTextAlignmentCenter);
+  text_layer_set_background_color(Morse_layer1, GColorClear);
+  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(Morse_layer1));
   
-  Time_layer = text_layer_create(GRect(0, 168-40, 144, 40));
-  text_layer_set_font(Time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-  text_layer_set_text_color(Time_layer, GColorWhite);
-  text_layer_set_background_color(Time_layer, GColorBlack);
+  Morse_layer2 = text_layer_create(GRect(0, 30, 144, 140));
+  text_layer_set_font(Morse_layer2, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(Morse_layer2, GTextAlignmentCenter);
+  text_layer_set_background_color(Morse_layer2, GColorClear);
+  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(Morse_layer2));
+  
+  Morse_layer3 = text_layer_create(GRect(0, 60, 144, 140));
+  text_layer_set_font(Morse_layer3, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(Morse_layer3, GTextAlignmentCenter);
+  text_layer_set_background_color(Morse_layer3, GColorClear);
+  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(Morse_layer3));
+  
+  Morse_layer4 = text_layer_create(GRect(0, 90, 144, 140));
+  text_layer_set_font(Morse_layer4, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(Morse_layer4, GTextAlignmentCenter);
+  text_layer_set_background_color(Morse_layer4, GColorClear);
+  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(Morse_layer4));
+
+  Time_layer = text_layer_create(GRect(0, 140, 144, 168));
+  text_layer_set_font(Time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
   text_layer_set_text_alignment(Time_layer, GTextAlignmentCenter);
+  text_layer_set_background_color(Time_layer, GColorBlack);
+  text_layer_set_text_color(Time_layer, GColorWhite);
+  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(Time_layer));
   
   time_t now = time(NULL);
   struct tm *current_time = localtime(&now);
-  handle_tick(current_time, MINUTE_UNIT);
-  tick_timer_service_subscribe(HOUR_UNIT|MINUTE_UNIT, &handle_tick);
+  UpdateTime(current_time);
   
-  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(Time_layer));
-  
-  Title_layer = text_layer_create(GRect(0, 0, 144, 35));
-  text_layer_set_font(Title_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-  text_layer_set_text_color(Title_layer, GColorWhite);
-  text_layer_set_background_color(Title_layer, GColorBlack);
-  text_layer_set_text_alignment(Title_layer, GTextAlignmentCenter);
-  text_layer_set_text(Title_layer, "Morse Code Clock");
-  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(Title_layer));
-  
+  tick_timer_service_subscribe(MINUTE_UNIT, &handle_tick);
   accel_tap_service_subscribe(&handle_tap);
   
   window_stack_push(my_window, true);
 }
 
 void handle_deinit(void) {
-  tick_timer_service_unsubscribe();
   accel_tap_service_unsubscribe();
+  tick_timer_service_unsubscribe();
   text_layer_destroy(Time_layer);
   window_destroy(my_window);
 }
